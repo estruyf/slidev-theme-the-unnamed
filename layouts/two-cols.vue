9586a371f6ca4cbf3509e7fd31fbdf058378e596
Elio Struyf <elio@struyfconsulting.be>
2023-10-17 21:13:38 +0200

    Update two columns

---
+++ layouts/two-cols.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="slidev-layout two-columns w-full h-full grid grid-cols-2"
+    :class="props.layoutClass"
+  >
+    <div class="col-left" :class="props.class">
+      <slot />
+    </div>
+
+    <div class="col-right" :class="props.class">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
