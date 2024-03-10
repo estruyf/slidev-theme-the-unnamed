8bdc2031ed87f6f332cf89b815c5f4e08d2e32cc
Florian Grandel <fgrandel@code-for-humans.de>
2024-03-10 12:39:09 +0100

    Fix shiki deprecation warning
    Starting slidev with the latest shiki version would produce the
    following warning:
    
    [slidev] `loadTheme` in `setup/shiki.ts` is deprecated. Pass directly
    the theme name it's supported by Shiki. For custom themes, load it
    manually via `JSON.parse(fs.readFileSync(path, 'utf-8'))` and pass the
    raw JSON object instead.
    
    This commit fixes the warning and also removes a redundant slash in
    develompment configuration.

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,12 +1,12 @@
 /* ./setup/shiki.ts */
 import { defineShikiSetup } from '@slidev/types'
+import fs from 'fs'
+import path from 'path'
 
-export default defineShikiSetup(async ({ loadTheme }) => {
-  const prefix = process.env.NODE_ENV === "development" ? "../" : "slidev-theme-the-unnamed";
+export default defineShikiSetup(() => {
+  const prefix = process.env.NODE_ENV === "development" ? ".." : "slidev-theme-the-unnamed";
+  const theme = JSON.parse(fs.readFileSync(path.resolve(__dirname, `${prefix}/public/theme/theunnamed-dark-theme.json`)));
   return {
-    theme: {
-      dark: await loadTheme(require.resolve(`${prefix}/public/theme/theunnamed-dark-theme.json`)),
-      light: await loadTheme(require.resolve(`${prefix}/public/theme/theunnamed-dark-theme.json`))
-    },
+    theme
   }
 })
