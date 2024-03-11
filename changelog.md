4eb38c4c38cafa266d162ed7fabfb19222441595
Elio Struyf <elio@struyfconsulting.be>
2024-03-11 10:27:05 +0100

    Updated changelog + small refactor

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.0.16] - 2024-03-11
+
+- [#5](https://github.com/estruyf/slidev-theme-the-unnamed/pull/5): Fix Shiki `loadTheme` deprecation warning. Thanks to [fgrandel](https://github.com/fgrandel).
+
 ## [0.0.15] - 2023-10-18
 
 - Added box shadow to the header + `header-shadow` theme option
--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,12 +1,18 @@
 /* ./setup/shiki.ts */
-import { defineShikiSetup } from '@slidev/types'
-import fs from 'fs'
-import path from 'path'
+import { defineShikiSetup } from "@slidev/types";
+import fs from "fs";
+import path from "path";
 
 export default defineShikiSetup(() => {
-  const prefix = process.env.NODE_ENV === "development" ? ".." : "slidev-theme-the-unnamed";
-  const theme = JSON.parse(fs.readFileSync(path.resolve(__dirname, `${prefix}/public/theme/theunnamed-dark-theme.json`)));
+  const prefix =
+    process.env.NODE_ENV === "development" ? ".." : "slidev-theme-the-unnamed";
+  const filePath = path.resolve(
+    __dirname,
+    `${prefix}/public/theme/theunnamed-dark-theme.json`
+  );
+  const contents = fs.readFileSync(filePath, "utf-8");
+  const theme = JSON.parse(contents);
   return {
-    theme
-  }
-})
+    theme,
+  };
+});
